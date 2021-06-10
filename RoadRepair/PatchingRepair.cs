8bd8e1308e5816325cdf575e191215e8237cdc40
Ntambiye <CR220331@charlesriverlabs.com>
2021-06-10 13:36:15 +0100

    more changws

---
+++ RoadRepair/PatchingRepair.cs
@@ -0,0 +1,26 @@
+﻿namespace RoadRepair
+{
+    /// <summary>
+    /// A patch is where you fix just the part of the road that is damaged.
+    /// </summary>
+    public class PatchingRepair
+    {
+        public PatchingRepair(Road road)
+        {
+            Patches = road.Potholes;
+            Depth = 0.1;
+        }
+
+        public int Patches { get; }
+
+        public double Depth { get; }
+
+        public double GetVolume()
+        {
+            // Assume that every patch has a 1 x 1 area
+            var volume = Patches * Depth;
+            return volume;
+        }
+
+    }
+}
